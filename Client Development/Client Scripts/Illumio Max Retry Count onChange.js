7994190ac3945b3b1f470de550112626a9428e5f
jaymeetmehta_crest <jaymeet.mehta@crestdatasys.com>
2022-11-03 10:56:08 +0530

    Format changes

--- Client Development/Client Scripts/Illumio Max Retry Count onChange.js
+++ Client Development/Client Scripts/Illumio Max Retry Count onChange.js
@@ -2,7 +2,7 @@ function onChange(control, oldValue, newValue, isLoading, isTemplate) {
     if (isLoading || newValue === '') {
         return;
     }
-	newValue = newValue.replaceAll(',','');
+    newValue = newValue.replaceAll(',','');
     var val = parseInt(newValue, 10);
     if (val < 0 || val > 100) {
         g_form.showFieldMsg('http_retry_count', 'Invalid value. Please enter valid value between 0-100', 'error');
